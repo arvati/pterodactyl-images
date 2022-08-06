ee5ca1fe11aaa4a5c2d7f54e64ac412ec794055e
Ademar Arvati Filho <arvati@hotmail.com>
2022-08-06 18:00:02 -0300

    omada controller

---
+++ software/omada/entrypoint.sh
@@ -0,0 +1,96 @@
+#!/bin/ash
+
+#
+# Copyright (c) 2021 Ademar Arvati Filho
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# 
+
+# Default the TZ environment variable to UTC.
+TZ=${TZ:-UTC}
+export TZ
+echo "INFO: Time zone set to '${TZ}'"
+
+# Set environment variable that holds the Internal Docker IP
+INTERNAL_IP=$(ip route get 1 | awk '{print $NF;exit}')
+export INTERNAL_IP
+
+# Switch to the container's working directory
+cd $HOME || exit 1
+
+# Set startup script file
+if [ -e $HOME/.profile ]; then
+    source $HOME/.profile
+fi
+
+# Include ./local/bin to path
+if [ -d "$HOME/.local/bin" ] ; then
+    export PATH="$HOME/.local/bin:$PATH"
+fi
+
+# Set prompt text and color
+export PS1='\033[1m\033[33mcontainer@pterodactyl:\w \033[0m'
+
+# Print Java version
+printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0mjava -version\n"
+java -version
+
+OMADA_DIR="${HOME:-/home/container}"
+TLS_1_11_ENABLED="${TLS_1_11_ENABLED:-false}"
+SHOW_SERVER_LOGS="${SHOW_SERVER_LOGS:-true}"
+SHOW_MONGODB_LOGS="${SHOW_MONGODB_LOGS:-false}"
+
+# validate permissions on /tmp
+TMP_PERMISSIONS="$(stat -c '%a' /tmp)"
+if [ "${TMP_PERMISSIONS}" != "1777" ]
+then
+  echo "WARN: permissions are not set correctly on '/tmp' (${TMP_PERMISSIONS}); setting correct permissions (1777)"
+  chmod -v 1777 /tmp
+fi
+
+# re-enable disabled TLS versions 1.0 & 1.1
+if [ "${TLS_1_11_ENABLED}" = "true" ]
+then
+  echo "INFO: Re-enabling TLS 1.0 & 1.1"
+  # openjdk17
+  sed -i 's#^jdk.tls.disabledAlgorithms=SSLv3, TLSv1, TLSv1.1,#jdk.tls.disabledAlgorithms=SSLv3,#' /etc/java-17-openjdk/security/java.security
+fi
+
+# Convert all of the "{{VARIABLE}}" parts of the command into the expected shell
+# variable format of "${VARIABLE}" before evaluating the string and automatically
+# replacing the values.
+PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
+
+# Display the command we're running in the output, and then execute it with the env
+# from the container itself.
+printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
+# shellcheck disable=SC2086
+#exec env ${PARSED}
+
+echo "INFO: Starting Omada Controller"
+
+if [ "${SHOW_SERVER_LOGS}" = "true" ]
+then
+  gosu omada tail -F -n 0 ${OMADA_DIR}/logs/server.log &
+fi
+if [ "${SHOW_MONGODB_LOGS}" = "true" ]
+then
+  gosu omada tail -F -n 0 ${OMADA_DIR}/logs/mongod.log &
+fi
+exec gosu container "${PARSED}"
---
+++ software/omada/install.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+set -e
+
+# omada controller package installer script for pterodactyl base image
+OMADA_VER="${OMADA_VER:-}"
+OMADA_TAR="${OMADA_TAR:-}"
+OMADA_URL="${OMADA_URL:-}"
+OMADA_MAJOR_VER="${OMADA_VER%.*.*}"
+OMADA_MAJOR_MINOR_VER="${OMADA_VER%.*}"
+OMADA_DIR="/opt/tplink/EAPController"
+
+cd /tmp
+chmod -v 1777 /tmp
+
+wget -nv "${OMADA_URL}"
+tar zxvf "${OMADA_TAR}"
+rm -f "${OMADA_TAR}"
+cd Omada_SDN_Controller_*
+
+mkdir -p "${OMADA_DIR}"
+NAMES=( bin data properties lib install.sh uninstall.sh )
+# copy over the files to the destination
+for NAME in "${NAMES[@]}"
+do
+  cp -r "${NAME}" "${OMADA_DIR}"
+done
+
+# symlink for mongod
+ln -sf "$(command -v mongod)" "${OMADA_DIR}/bin/mongod"
+chmod 755 "${OMADA_DIR}"/bin/*
+
+mkdir -p "${OMADA_DIR}/logs" 
+mkdir -p "${OMADA_DIR}/work"
+mkdir -p "${OMADA_DIR}/data/pdf"
+mkdir -p "${OMADA_DIR}/data/db"
+mkdir -p "${OMADA_DIR}/data/keystore"
+chown -R container:container "${OMADA_DIR}"
+
+if [ -d ${OMADA_DIR} ]
+then
+  # create backup
+  cd ${OMADA_DIR}
+  tar zcvf /omada.tar.gz .
+fi
+
+# echo "**** Cleanup ****"
+rm -rf /tmp/* ${OMADA_DIR}
